13a4c3e75ac4ff8a36a6c109b363e15087732c42
drielison.lopes <80991091@telefonicati.onmicrosoft.com>
2026-02-24 17:08:28 -0300

    feat: added authentication to cities and people endpoints

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -4,6 +4,7 @@ import {
     PeopleController,
     UsersController,
 } from "./../controllers";
+import { ensureAuthenticated } from "../shared/middleware";
 
 const router = Router();
 
@@ -30,26 +31,31 @@ router.get("/", (_, res) => {
 // Cities routes
 router.get(
     "/cities",
+    ensureAuthenticated,
     CitiesController.getAllValidation,
     CitiesController.getAll,
 );
 router.get(
     "/cities/:id",
+    ensureAuthenticated,
     CitiesController.getByIdValidation,
     CitiesController.getById,
 );
 router.post(
     "/cities",
+    ensureAuthenticated,
     CitiesController.createValidation,
     CitiesController.create,
 );
 router.put(
     "/cities/:id",
+    ensureAuthenticated,
     CitiesController.updateByIdValidation,
     CitiesController.updateById,
 );
 router.delete(
     "/cities/:id",
+    ensureAuthenticated,
     CitiesController.deleteByIdValidation,
     CitiesController.deleteById,
 );
@@ -57,26 +63,31 @@ router.delete(
 // People routes
 router.get(
     "/people",
+    ensureAuthenticated,
     PeopleController.getAllValidation,
     PeopleController.getAll,
 );
 router.get(
     "/people/:id",
+    ensureAuthenticated,
     PeopleController.getByIdValidation,
     PeopleController.getById,
 );
 router.post(
     "/people",
+    ensureAuthenticated,
     PeopleController.createValidation,
     PeopleController.create,
 );
 router.put(
     "/people/:id",
+    ensureAuthenticated,
     PeopleController.updateByIdValidation,
     PeopleController.updateById,
 );
 router.delete(
     "/people/:id",
+    ensureAuthenticated,
     PeopleController.deleteByIdValidation,
     PeopleController.deleteById,
 );
